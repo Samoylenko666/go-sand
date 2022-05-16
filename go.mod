e6c96abb11e0ac805a0694a2965f79061dc5aa1e
Samoylenko666 <dsamoylenko@itl.agency>
2022-05-16 17:01:07 +0600

    try implement register method

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/lib/pq v1.10.5 // indirect
+	golang.org/x/crypto v0.0.0-20220513210258-46612604a0f9 // indirect
 )
--- main.go
+++ main.go
@@ -7,10 +7,15 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/davecgh/go-spew/spew"
+	"golang.org/x/crypto/bcrypt"
+	//"github.com/davecgh/go-spew/spew"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
+
+
+// DB is a global variable to hold db connection
+var DB *sql.DB
 
 const (
 	host     = "localhost"
@@ -43,13 +48,19 @@ func main() {
 		host, port, user, password, dbname)
 
 	db, err := sql.Open("postgres", psqlInfo)
-	defer db.Close()
+	//defer db.Close()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//fmt.Println(db)
+	err = db.Ping()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DB = db
 
 	router := mux.NewRouter()
 
@@ -64,20 +75,22 @@ func main() {
 
 }
 
+func responseWithError(response http.ResponseWriter, status int, error Error) {
+
+	response.WriteHeader(http.StatusBadRequest)
+	json.NewEncoder(response).Encode(error)
+}
+
+func responseWithSucces(response http.ResponseWriter, data interface{}) {
+	response.Header().Set("Content-type", "application/json")
+	json.NewEncoder(response).Encode(data)
+}
+
 func index(response http.ResponseWriter, request *http.Request) {
 
 	fmt.Println("Invoked index method")
 
 	response.Write([]byte("Success cc fdf ! ds><> "))
-
-}
-
-func responseWithError(response http.ResponseWriter, status int, error Error) {
-
-	error.Message = "Email is missing"
-	response.WriteHeader(http.StatusBadRequest)
-	json.NewEncoder(response).Encode(error)
-
 
 }
 
@@ -90,28 +103,43 @@ func register(response http.ResponseWriter, request *http.Request) {
 
 	if user.Email == "" {
 		error.Message = "Email is missing"
-		response.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(response).Encode(error)
-
+		responseWithError(response, http.StatusBadRequest, error)
 		return
 	}
 
 	if user.Password == "" {
 		error.Message = "Password is missing"
-		response.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(response).Encode(error)
-
+		responseWithError(response, http.StatusBadRequest, error)
 		return
 	}
 
 	if user.Name == "" {
 		error.Message = "Message is missing"
-		response.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(response).Encode(error)
-
+		responseWithError(response, http.StatusBadRequest, error)
 		return
 	}
 
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user.Password = string(hash)
+
+	stmt := "insert into users (email, password, name) values($1, $2, $3) RETURNING id;"
+
+	err = DB.QueryRow(stmt, user.Email, user.Password, user.Name).Scan(&user.Id)
+
+	if err != nil {
+		error.Message = "Something went wrong"
+
+		responseWithError(response, http.StatusInternalServerError, error)
+	}
+
+	user.Password = ""
+
+	responseWithSucces(response,user)
 }
 
 func login(response http.ResponseWriter, request *http.Request) {
